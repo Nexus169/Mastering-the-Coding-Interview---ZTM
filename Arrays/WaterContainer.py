ab84430008f9dfef645bed484c289fe9cd72a8e8
Nexus13 <18453379+Nexus169@users.noreply.github.com>
2023-10-02 21:07:51 -0500

    Adding some extra comments and formatting

--- Arrays/WaterContainer.py
+++ Arrays/WaterContainer.py
@@ -20,12 +20,12 @@ def get_max_water_container(height):
     right_side = len(height) - 1
 
     while left_side < right_side:
-        short_side = min(height[left_side], height[right_side])
+        short_side = min(height[left_side], height[right_side])     # determine which side is the shorter height, then calculate current area of container
         current_area = short_side * (right_side - left_side)
 
-        max_area = max(max_area, current_area)
+        max_area = max(max_area, current_area)                      # update our current found max area
 
-        if height[left_side] <= height[right_side]:
+        if height[left_side] <= height[right_side]:                 # advance whichever side had the shorter height closer to the middle
             left_side += 1
         else: right_side -= 1
 
@@ -33,18 +33,20 @@ def get_max_water_container(height):
 
 # Some tests
 
-heights_empty = []
-heights_single = [5]
-heights = [7, 1, 2, 3, 9]
-heights_2 = [4, 8, 1, 2, 3, 9]
+heights_empty = []                  # Expected: 0
+heights_single = [5]                # Expected: 0
+heights = [7, 1, 2, 3, 9]           # Expected: 28
+heights_2 = [4, 8, 1, 2, 3, 9]      # Expected: 32
 
 # with optimal solution
+print("Optimal Solution:")
 print(get_max_water_container(heights_empty))
 print(get_max_water_container(heights_single))
 print(get_max_water_container(heights))
 print(get_max_water_container(heights_2))
 
 # with brute force
+print("Brute Force:")
 print(get_max_water_container_bf(heights_empty))
 print(get_max_water_container_bf(heights_single))
 print(get_max_water_container_bf(heights))
